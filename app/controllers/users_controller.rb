5eb0c4c981e7affb968be709225bf4b436b1a542
ReidasaurusRex <reidhlewis91@gmail.com>
2015-03-29 12:04:10 -0700

    Slimmed down unnecessary files from generating. Added new user form, funcionality. Still in work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new user_params
+    redirect_to home_path
   end
 
   def home
@@ -19,5 +21,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_digest)
   end
 end
--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -1,1 +1,20 @@
 require 'rails_helper'
+
+feature 'Visitor signs up' do
+  scenario 'goes to sign up page' do
+    # Setup
+    page.set_rack_session(user_id: nil)
+    visit root_path
+
+    # Exercise
+    click_button "Sign Up with Email"
+
+    # Verify
+    expect(page).to have_content("First name")
+    expect(page).to have_content("Last name")
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+    expect(page).to have_content("Confirm Password")
+  end
+
+end
