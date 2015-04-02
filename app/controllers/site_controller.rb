0c3375178636ac1995038780d15972c56b109ee1
ReidasaurusRex <reidhlewis91@gmail.com>
2015-04-02 09:57:58 -0700

    session reset

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,7 @@
 class SiteController < ApplicationController
-  before_action :logged_in_redirect, only: :landing
+  # before_action :logged_in_redirect, only: :landing
   def landing
+    reset_session
   end
 
   def about
